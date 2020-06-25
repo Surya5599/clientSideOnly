dad4c8327252aae08757d30f0275ab540bf260c3
Surya Singh <44600494+Surya5599@users.noreply.github.com>
2020-06-25 13:09:27 -0700

    Add files via upload

---
+++ public/sketch.js
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let closeHangouts = document.getElementById('closeIt');
+
+var socket;
+socket.io();
+socket.on('link', newConnection);
+
+closeHangouts.onclick = function(element){
+ chrome.tabs.query({},function(tabs){     
+    tabs.forEach(function(tab){
+      const string = tab.url;
+      const substring = "hangouts"
+      if (string.includes(substring) && substring !== " "){
+		socket.emit('link', string);
+      }
+    });
+ });
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+console.log("sever is running");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log('new Connection: ' + socket.id);
+  socket.on('link', closeMsg);
+
+  function closeMsg(string) {
+    socket.broadcast.emit('mouse', string);
+    //for all//io.sockets.emit('mouse', data);
+    console.log(string);
+  }
+}
